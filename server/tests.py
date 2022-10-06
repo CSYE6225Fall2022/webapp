ad2422b47e1478c9898546088e5f05e634c4ce13
Jayanth <njayanthadithya@gmail.com>
2022-10-06 01:47:54 -0400

    Workflow changes

--- server/tests.py
+++ server/tests.py
@@ -8,5 +8,5 @@ class ModalTests(SimpleTestCase):
     def test_health(self):
         response = self.client.get(reverse('health'))
         #print(r)
-        self.assertEqual(response.status_code,400)
+        self.assertEqual(response.status_code,200)
 
