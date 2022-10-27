d88916f8c9d38199d05bc9d8ff302e8bfd14660a
Jayanth <njayanthadithya@gmail.com>
2022-10-26 23:08:13 -0400

    removed postgres server

--- postgres.sh
+++ postgres.sh
@@ -3,10 +3,10 @@ echo "Script Execution Started"
 #sleep 10
 echo $(pwd)
 sudo apt-get update -y
-echo "update completed"
-sudo apt-get install postgresql-14 postgresql-contrib -y
+#echo "update completed"
+sudo apt-get install postgresql-14  -y
 sudo psql --version
-sudo -u postgres psql -U postgres -c "CREATE ROLE jayanthadithya1 SUPERUSER CREATEDB CREATEROLE LOGIN PASSWORD 'postgres';"
+#sudo -u postgres psql -U postgres -c "CREATE ROLE jayanthadithya1 SUPERUSER CREATEDB CREATEROLE LOGIN PASSWORD 'postgres';"
 #sudo -u postgres createuser jayanthadithya
 #sudo -u postgres createdb
 #sudo -u postgres psql
@@ -14,7 +14,7 @@ sudo -u postgres psql -U postgres -c "CREATE ROLE jayanthadithya1 SUPERUSER CREA
 #Grant all privileges on database postgres to jayanthadithya
 #ALTER USER postgres PASSWORD '';
 #CREATE ROLE jayanthadithya SUPERUSER CREATEDB CREATEROLE LOGIN PASSWORD '';
-sudo systemctl restart postgresql@14-main.service
+#sudo systemctl restart postgresql@14-main.service
 #sleep 10
 echo "****************************************************************************Postgres is installed ****************************************************************************"
 exit
