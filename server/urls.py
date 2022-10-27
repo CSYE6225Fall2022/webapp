166f8abc072293ea79e833809879d8c2ae40e525
Jayanth <njayanthadithya@gmail.com>
2022-10-27 01:49:52 -0400

    testcase

--- server/urls.py
+++ server/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from . views import Health
 urlpatterns = [
-    path('',views.health,name='health'),
+    path('',Health.as_view(),name='health'),
 ]
